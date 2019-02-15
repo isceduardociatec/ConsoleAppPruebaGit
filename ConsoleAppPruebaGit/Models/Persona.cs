f259cb77995492bb6aec85d9f8befbf5c14c31f3
Eduardo <Eduardo@DESKTOP-QU9R0GI>
2019-02-15 15:53:23 -0600

    Model Persona

--- ConsoleAppPruebaGit/Models/Persona.cs
+++ ConsoleAppPruebaGit/Models/Persona.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppPruebaGit.Models
 {
-    class Persona
+    public class Persona
     {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Post { get; set; }
+        public int Velitas { get; set; }
+        public int Altares { get; set; }
     }
 }
